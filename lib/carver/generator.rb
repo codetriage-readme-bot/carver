68d40ab85e5d5af9ec0e4e7877d378f299ef4d75
Vinicius Stock <vinicius.stock@outlook.com>
2017-08-22 18:25:31 -0300

    Start sorting results by allocated memory, descending

--- lib/carver/generator.rb
+++ lib/carver/generator.rb
@@ -4,8 +4,8 @@ require 'erb'
 module Carver
   class Generator
     def initialize(results, output_path)
-      @controller_results = results.select { |item| item.include?('Controller') }
-      @job_results = results.select { |item| item.include?('Job') }
+      @controller_results = sort(results.select { |item| item.include?('Controller') })
+      @job_results = sort(results.select { |item| item.include?('Job') })
       @output_path = output_path
     end
 
@@ -13,6 +13,16 @@ module Carver
       template = File.read("#{File.expand_path('..', __FILE__)}/templates/results.html.erb")
       html = ERB.new(template).result(binding)
       File.open(@output_path, 'w') { |f| f.write(html) }
+    end
+
+    private
+
+    def sort(results)
+      results.each do |key, value|
+        results[key].replace(value.sort_by { |a| -a[:total_allocated_memsize] })
+      end
+
+      results.sort_by { |_, value| -value.first[:total_allocated_memsize] }.to_h
     end
   end
 end
--- spec/carver/generator_spec.rb
+++ spec/carver/generator_spec.rb
@@ -1,20 +1,33 @@
 # frozen_string_literal: true
 describe Carver::Generator do
-  describe '.create_html_from' do
+  let(:output_file) { './profiling/results.html' }
+  let(:results) { {
+      'Api::V1::ExamplesController#index' => [
+          { total_allocated_memsize: 21000, total_retained_memsize: 1500 },
+          { total_allocated_memsize: 22150, total_retained_memsize: 1450 }
+      ],
+      'Api::V1::HerpController#index' => [
+          { total_allocated_memsize: 33000, total_retained_memsize: 1500 },
+          { total_allocated_memsize: 25000, total_retained_memsize: 1450 }
+      ],
+      'ExamplesJob#perform' => [
+          { total_allocated_memsize: 5700, total_retained_memsize: 800 }
+      ] } }
+
+  describe '.initialize' do
+    subject { described_class.new(results, output_file) }
+
+    it 'orders results' do
+      expect(subject.instance_variable_get(:@controller_results).keys).to eq(%w(Api::V1::HerpController#index Api::V1::ExamplesController#index))
+
+      subject.instance_variable_get(:@controller_results).each do |_, value|
+        expect(value.first[:total_allocated_memsize]).to be > value.second[:total_allocated_memsize]
+      end
+    end
+  end
+
+  describe '#create_html' do
     subject { described_class.new(results, output_file).create_html }
-    let(:output_file) { './profiling/results.html' }
-    let(:results) { {
-        'Api::V1::ExamplesController#index' => [
-            { 'total_allocated_memsize' => 21000, 'total_retained_memsize' => 1500 },
-            { 'total_allocated_memsize' => 22150, 'total_retained_memsize' => 1450 }
-        ],
-        'Api::V1::HerpController#index' => [
-            { 'total_allocated_memsize' => 21000, 'total_retained_memsize' => 1500 },
-            { 'total_allocated_memsize' => 22150, 'total_retained_memsize' => 1450 }
-        ],
-        'ExamplesJob#perform' => [
-            { 'total_allocated_memsize' => 5700, 'total_retained_memsize' => 800 }
-        ] } }
 
     it 'creates html from results' do
       expect(File).to receive(:open).with(output_file, 'w')
