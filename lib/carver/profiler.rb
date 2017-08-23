def64bb319c0d6c3643168b3201eecac951be1a8
Vinicius Stock <instantink+pair+vinicius@hp.com>
2017-08-23 14:03:58 -0300

    Bump version

--- lib/carver/profiler.rb
+++ lib/carver/profiler.rb
@@ -5,7 +5,7 @@ module Carver
       report = MemoryProfiler.report do
         yield
       end
-      
+
       presenter = Presenter.new(report, path, action, parent)
       presenter.log if Carver.configuration.log_results
       presenter.add_to_results if Carver.configuration.enabled
